770b5438e973b7c16ba346b76609e69527e24fbd
Sydney Keating <bookmasterdc@gmail.com>
2021-01-03 00:52:20 -0500

    Forgot two files.

---
+++ ConfirmEmployee.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ConfirmEmployee : MonoBehaviour
+{
+    public GameObject currentEmployeeCard;
+    public Button thisbutton;
+    public GameObject employeeObject;
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentEmployeeCard = SelectPay.currentEmployeeCard;
+        thisbutton = this.gameObject.GetComponent<Button>();
+        thisbutton.onClick.AddListener(confirm); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CreateEmployee.employeeCreated)
+        {
+            reassign();
+        }
+    }
+
+    public void confirm()
+    {
+        currentEmployeeCard.GetComponent<Animator>().Play("SlideOut");
+        this.gameObject.SetActive(false);
+    }
+
+     public void reassign()
+    {
+        currentEmployeeCard = SelectPay.currentEmployeeCard;
+        CreateEmployee.employeeCreated = false;
+      
+    }
+
+
+}
---
+++ SelectPay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SelectPay : MonoBehaviour
+{
+
+    public Button thisButton;
+    public static TextMeshProUGUI payText;
+    public GameObject confirm;
+    public static GameObject currentEmployeeCard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisButton = assignlistener(thisButton);
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    public Button assignlistener(Button button)
+    {
+        button.onClick.AddListener(setPay);
+
+        return button;
+    }
+
+    void setPay()
+    {
+        payText = currentEmployeeCard.GetComponent<CurrentEmployee>().hourlypay;
+        payText.text = "Hourly Pay: " + this.GetComponent<TextMeshProUGUI>().text;
+        confirm.SetActive(true);
+    }
+}
