a329b2b4de3ec3c0555306d1a364ce924610f991
Sydney Keating <bookmasterdc@gmail.com>
2021-01-14 18:51:07 -0500

    Generating tweets

---
+++ GetTweetText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GetTweetText : MonoBehaviour
+{
+    public static ArrayList paths = new ArrayList();
+    public string currentpath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paths.Add(@"\Assets\BookTextFiles\1001CorporateTweets.txt");
+        paths.Add(@"\Assets\BookTextFiles\ANTIFAHANDBOOK.txt");
+        System.Random rand = new System.Random();
+        currentpath = (string)paths[rand.Next(0, paths.Count)];
+        generateText(currentpath);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void generateText(string path)
+    {
+        Debug.Log(path);
+    }
+}
